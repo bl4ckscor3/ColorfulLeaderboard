d29006c8b16f4980705d160251c1099beafbb3f2
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-08-24 21:06:10 +0200

    use italic/underline instead of emojis for mod/mentor

--- colorfulleaderboard.user.js
+++ colorfulleaderboard.user.js
@@ -21,16 +21,12 @@
         scout: "#95fbe4",
         scythe: "#21a9ed",
         mystic: "#ff6666",
-        //moderator: "#9518f2",
-        //mentor: "#ee57ff",
         admin: "#ffea49"
     };
     const leaderboardObserver = new MutationObserver(colorPlayers);
-    //key: playername, value: {color, text}
+    //key: playername, value: {color, isMod, isMentor}
     const loadedPlayers = new Map();
 
-    resizeLeaderboard();
-    resizeCubesPanel();
     leaderboardObserver.observe(document.getElementById("leaderboard"), {
         childList: true,
         attributes: false,
@@ -47,10 +43,6 @@
         let request = leaderboard.country ? `/1.0/stats/top/user/in/country/${leaderboard.country}/by/points/per/` : "/1.0/stats/top/user/by/points/per/";
 
         $.getJSON(request + leaderboard.timeframe, function(data) {
-            if(leaderboard.competition_id !== null) { //do not color names if a competition is displayed
-                return;
-            }
-
             let mutationsIndex = 0;
 
             for(let i = 0; i < data.length; i++) {
@@ -58,7 +50,13 @@
                     mutationsIndex++;
                 }
 
-                processPlayer(data, mutations[mutationsIndex++], data[i].username);
+                let indexToUse = mutationsIndex++;
+
+                if(!mutations[indexToUse]) {
+                    continue;
+                }
+
+                processPlayer(data, mutations[indexToUse], data[i].username);
             }
         });
     }
@@ -72,16 +70,14 @@
         let nameSpan = entry.childNodes[1]; //<span><span>name</span><img ...></span>
 
         if(loadedPlayers.has(name)) {
-            let playerData = loadedPlayers.get(name);
-
-            updatePlayerColor(nameSpan, playerData.color);
-            updatePlayerText(nameSpan, playerData.text);
+            updatePlayer(nameSpan, loadedPlayers.get(name));
         }
         else {
             $.getJSON(`/1.0/player/${name}/bio`, function(data) {
                 let color = Colors.player;
                 let roles = data.roles;
-                let text = "";
+                let isMod = false;
+                let isMentor = false;
 
                 if(roles.length !== 0) {
                     let isAdmin = false;
@@ -101,101 +97,52 @@
                     }
 
                     if(roles.includes("moderator") || isAdmin) { //always show the icon for admins
-                        text += "📖 "; //space so that the potential mentor emoji has a little breathing space
+                        isMod = true;
                     }
 
                     if(roles.includes("mentor") || isAdmin) { //always show the icon for admins
-                        text += "🗨";
+                        isMentor = true;
                     }
                 }
 
-                savePlayer(nameSpan, name, {color: color, text: text});
+                saveAndUpdatePlayer(nameSpan, name, {color: color, isMod: isMod, isMentor: isMentor});
             }).fail(function() {
                 console.warn(`Failed to color ${name} in the leaderboard, falling back to default player color.`);
-                savePlayer(nameSpan, name, {color: Colors.player, text: ""});
+                saveAndUpdatePlayer(nameSpan, name, {color: Colors.player, isMod: false, isMentor: false});
             });
         }
     }
 
-    function savePlayer(element, playerName, data) {
-        updatePlayerColor(element, data.color);
-        updatePlayerText(element, data.text);
+    function saveAndUpdatePlayer(element, playerName, data) {
+        updatePlayer(element, data);
         loadedPlayers.set(playerName, data);
     }
 
-    function updatePlayerColor(element, color) {
-        element.childNodes[0].style.color = color;
+    function updatePlayer(element, data) {
+        updatePlayerColor(element, data.color);
+        updatePlayerInfo(element, data.isMod, data.isMentor);
     }
 
-    function updatePlayerText(element, text) {
-        if(text) { //only modify if the player has text to display, aka if they're a mod or a mentor
-            let data = loadedPlayers.get(name);
-            let icons = element.getElementsByClassName("modmentorindicator");
-
-            if(icons.length === 0) {
-                let flagImg = element.childNodes[1] ? element.childNodes[1].cloneNode() : null; //not all users have a flag set
-                let iconSpan = document.createElement("span");
-                let flagIconSpan = document.createElement("span");
-
-                if(flagImg) {
-                    element.childNodes[1].remove(); //remove the old img node
-                    flagIconSpan.appendChild(flagImg);
-                }
-
-                iconSpan.setAttribute("class", "modmentorindicator");
-                iconSpan.setAttribute("style", "padding-left: 5px;");
-                iconSpan.innerText = text;
-                flagIconSpan.appendChild(iconSpan);
-                element.appendChild(flagIconSpan);
-            }
-            else {
-                icons.innerText = text;
-            }
+    function updatePlayerColor(element, color) {
+        if(leaderboard.competition_id === null) { //do not color names during competitions
+            element.getElementsByTagName("span")[0].style.color = color;
         }
     }
 
-    function resizeLeaderboard() {
-        const widerLeaderboardStyle = document.createElement("style");
-        const usernameColumnWidth = ".leaderRowHeader span:nth-child(2), .leaderRow span:nth-child(2) {width: 160px;}"; //increase username column width
-        const wideLeaderboard = "#ovlbContainer {width: 310px}"; //increase leaderboard width
-        const normalLeaderboard = "#ovlbContainer {width: 280px}";
+    function updatePlayerInfo(element, isMod, isMentor) {
+        if(leaderboard.competition_id === null) { //do not style names during competitions
+            //mark the name italics and underline for mod/mentor
+            var textStyle = `color: ${element.childNodes[0].style.color};`;
 
-        widerLeaderboardStyle.setAttribute("id", "widerLeaderboard");
-        widerLeaderboardStyle.innerHTML = usernameColumnWidth + wideLeaderboard;
-        document.head.appendChild(widerLeaderboardStyle);
-
-        //fixes the leaderboard not completely disappearing off the screen when dismissed
-        document.getElementById("dismiss-leaderboard").onclick = function() {
-            widerLeaderboardStyle.innerHTML = usernameColumnWidth + normalLeaderboard;
-        };
-        document.getElementById("recall-leaderboard").onclick = function() {
-            widerLeaderboardStyle.innerHTML = usernameColumnWidth + wideLeaderboard;
-        };
-    }
-
-    function resizeCubesPanel() {
-        const playerAddons = JSON.parse(localStorage.getItem("playerAddons")); //used to check if the cubes addon is enabled so its display can be widened as well
-
-        if(playerAddons) {
-            for(let addon of playerAddons) {
-                if(addon.name === "Cubes") {
-                    const interval = setInterval(function() {
-                        let cubesPanel = document.getElementById("ews-cubes-panel");
-
-                        if(cubesPanel) {
-                            clearInterval(interval);
-
-                            cubesPanel.style.width = "310px"; //make the cubes script panel the same width as the leaderboard
-
-                            for(let tab of cubesPanel.getElementsByClassName("ews-cubes-tab")) {
-                                tab.setAttribute("style", "margin-left: 6px"); //add more space between the tab buttons
-                            }
-                        }
-                    }, 100);
-
-                    return;
-                }
+            if(isMod) {
+                textStyle += "font-style: italic;";
             }
+
+            if(isMentor) {
+                textStyle += "text-decoration: underline;";
+            }
+
+            element.childNodes[0].style = textStyle;
         }
     }
 })();
